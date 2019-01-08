7e11d141e5cf6b42ad1128766434e20f102a442c
Pierre Pavlovic <34651188+Piheros@users.noreply.github.com>
2019-01-08 19:27:14 +0400

    Media center application without details of images
    Works update, supress and create

---
+++ WebApplication1/WebApplication1/Data/DbContextDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class DbContextDemo : DbContext
+    {
+        
+        public DbContextDemo(DbContextOptions<DbContextDemo> options) : base(options) { }
+
+        public DbSet<Galerie> Galerie {get; set;}
+        
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Galerie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Galerie
+    {
+        public string Nom { get; set; }
+        public int Id { get; set; }
+
+    }
+}
